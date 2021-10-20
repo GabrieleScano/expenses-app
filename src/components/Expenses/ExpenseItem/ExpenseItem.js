a87bad80089bc7a669566d113b3cd90dd5d8a35d
gabri <gabrielescano.bcn@gmail.com>
2021-10-20 22:23:57 +0200

    add conditional return

--- src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -6,12 +6,14 @@ import './ExpenseItem.css'
 export const ExpenseItem = ({date, title, amount}) => {
 
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={date}/>
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">€ {amount}</div>
-            </div>
-        </Card>
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={date} />
+                <div className="expense-item__description">
+                    <h2>{title}</h2>
+                    <div className="expense-item__price">€ {amount}</div>
+                </div>
+            </Card>
+        </li>
     )
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
-import { ExpenseItem } from './ExpenseItem/ExpenseItem'
 import { Card } from '../UI/Card'
 import { ExpensesFilter } from '../ExpensesFilter/ExpensesFilter'
 import './Expenses.css'
+import { ExpensesList } from './ExpensesList/ExpensesList'
 
 export const Expenses = (props) => {
 
@@ -15,20 +15,6 @@ export const Expenses = (props) => {
   const filteredExpenses = props.items.filter( expense => {
     return expense.date.getFullYear().toString() === filteredYear
   })
-
-  let expensesContent = <p>No expenses found.</p>
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => 
-    (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))
-  }
   
   return (
     <>
@@ -37,7 +23,7 @@ export const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
       <Card className="expenses">
-        {expensesContent}
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </>
   );
