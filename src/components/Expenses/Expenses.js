e2e306caaf71dc60a658fea28c44301ef8f93d54
geso <gabriele.scano@gft.com>
2021-10-20 16:08:14 +0200

    add filter method

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,6 +10,11 @@ export const Expenses = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter( expense => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
   return (
     <>
       <Card className="expenses">
@@ -18,9 +23,10 @@ export const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
         {
-          props.items.map((expense) => {
+          filteredExpenses.map((expense) => {
             return (
               <ExpenseItem
+                key={expense.id}
                 title={expense.title}
                 amount={expense.amount}
                 date={expense.date}
