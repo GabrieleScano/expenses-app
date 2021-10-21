0368de4ceda8b99cd14faf09d4565efb33a76791
geso <gabriele.scano@gft.com>
2021-10-21 09:59:08 +0200

    fix bug

--- src/components/NewExpense/ExpenseForm/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm/ExpenseForm.js
@@ -24,7 +24,7 @@ export const ExpenseForm = (props) => {
 
         const expenseData = {
             title: enteredTitle,
-            amount: enteredAmount,
+            amount: +enteredAmount,
             date: new Date(enteredDate)
         } 
 
